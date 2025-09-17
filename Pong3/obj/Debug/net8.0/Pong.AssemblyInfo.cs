8c9bbb0093303a6abf37ff34bcf8a0b1c23c4566
sjoerdmpfrgh <sjoerdmpfrgh@gmail.com>
2025-09-17 16:13:44 +0200

    hi

--- Pong3/obj/Debug/net8.0/Pong.AssemblyInfo.cs
+++ Pong3/obj/Debug/net8.0/Pong.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66efdf24b6f7dc96a6dd81a1cff3a6b3de34a5e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
